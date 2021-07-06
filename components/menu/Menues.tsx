6656af6328beeaeaf9fed700024eb2550897f395
MASANORI-M <kougakumasa.1031@gmail.com>
2021-07-07 00:05:55 +0900

    更新

--- components/menu/Menues.tsx
+++ components/menu/Menues.tsx
@@ -7,14 +7,20 @@ import {
     SubMenu
 } from '@szhsin/react-menu';
 import '@szhsin/react-menu/dist/index.css';
+import link from 'next/link';
 
 const Menues = () => {
     return (
         <div>
             <Menu menuButton={<MenuButton><Image width="70" height="70" src="/login.png" alt=""></Image></MenuButton>}>
-                <MenuItem>MY BOOK</MenuItem>
-                <MenuItem>EVALUTION</MenuItem>
-                <MenuItem>LOGIN</MenuItem>
+                <MenuItem>
+                    <a href="../../pages/mypage/MyReview">
+                        MY PAGE
+                    </a>
+                </MenuItem>
+                <MenuItem>PROFILE</MenuItem>
+                <MenuItem>LOGOUT</MenuItem>
+                <MenuItem>退会</MenuItem>
             </Menu>
         </div>
     );
