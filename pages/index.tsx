b9c085b556c2e250229345a833c803670191238e
MASANORI-M <kougakumasa.1031@gmail.com>
2021-07-27 23:51:37 +0900

    LP画面修正_1

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ import Carousel from '../components/lp/carousel';
 import CarouselBottom from '../components/lp/CarouselBottom';
 import Button from '../components/button/Button';
 
+//無視?
 const Home = () => {
   const { register, handleSubmit, watch, formState: { errors }, getValues, } = useForm();
   const [enabled, setEnabled] = useState<boolean>(false);
@@ -30,6 +31,7 @@ const Home = () => {
           <div className="border-b-4 border-fuchsia-600 border-gray-600 pt-24 pb-4">
             <h2 className="text-gray-600 italic font-bold text-4xl text-center tracking-widest">NEW EVALUATION BOOK</h2>
           </div>
+          {/* Google APIより本の画像を取得 */}
           <Carousel />
         </div>
       </div>
@@ -86,12 +88,65 @@ const Home = () => {
               <p className="text-1xl mt-12 tracking-widest leading-8">
                 なぜこの本を読もうと思ったのか、<br />
                 なぜこの本の中身が素晴らしいと感じたのか、<br />
-                あなたの持つ"本の価値"を発信できます。
+                <a href="/review/ReviewTop">あなたの持つ"本の価値"を発信できます。</a>
               </p>
             </div>
           </div>
         </div>
       </div>
+
+      {/* 画像はダミーでおいてある。後に差し替え */}
+      <div className=" bg-gray-100 m-auto pt-44 pb-44 text-center text-gray-600 block">
+          <div className="border-b-4 border-fuchsia-600 border-gray-600 pt-4 pb-4">
+              <h2 className="text-gray-600 italic font-bold text-4xl text-center tracking-widest">
+                  サービスのご利用方法
+              </h2>
+          </div>
+      </div>
+      <div className="bg-gray-100">
+          <div className="container">
+              <div className="grid py-40 lg:grid-cols-2 items-center">
+                  <div className="text-center">
+                      <Image width="350" height="350" className="rounded-lg shadow-2xl" src="/value1.jpg" alt=""></Image>
+                  </div>
+                  <div className="text-gray-600 text-center">
+                      <h3 className="text-2xl font-bold">1. 評価したい本を検索する</h3>
+                      <p className="text-1xl mt-12 tracking-widest leading-8">
+                          なぜこの本を読もうと思ったのか、<br />
+                          なぜこの本の中身が素晴らしいと感じたのか、<br />
+                          あなたの持つ"本の価値"を発信できます。
+                      </p>
+                  </div>
+              </div>
+              <div className="grid py-40 lg:grid-cols-2 items-center">
+                  <div className="text-center">
+                      <Image width="350" height="350" className="rounded-lg shadow-2xl" src="/value2.jpg" alt=""></Image>
+                  </div>
+                  <div className="text-gray-600 text-center">
+                      <h3 className="text-2xl font-bold">2. 本を評価をする</h3>
+                      <p className="text-1xl mt-12 tracking-widest leading-8">
+                          なぜこの本を読もうと思ったのか、<br />
+                          なぜこの本の中身が素晴らしいと感じたのか、<br />
+                          あなたの持つ"本の価値"を発信できます。
+                      </p>
+                  </div>
+              </div>
+              <div className="grid py-40 lg:grid-cols-2 items-center">
+                  <div className="text-center">
+                      <Image width="350" height="350" className="rounded-lg shadow-2xl" src="/value3.jpg" alt=""></Image>
+                  </div>
+                  <div className="text-gray-600 text-center">
+                      <h3 className="text-2xl font-bold">3. 投稿する</h3>
+                      <p className="text-1xl mt-12 tracking-widest leading-8">
+                          なぜこの本を読もうと思ったのか、<br />
+                          なぜこの本の中身が素晴らしいと感じたのか、<br />
+                          あなたの持つ"本の価値"を発信できます。
+                      </p>
+                  </div>
+              </div>
+          </div>
+      </div>
+
       <div className="bg-gray-100">
         <div className="container pb-24">
           <div className="border-b-4 border-fuchsia-600 border-gray-600 pt-24 pb-4">
@@ -100,7 +155,7 @@ const Home = () => {
           <div className="text-center">
             <div className="text-gray-600">
               <p className="font-bold text-2xl mt-20 mb-20 tracking-widest leading-relaxed">
-                本好きのあなたに送る、最高の時間を楽しもう!!
+              <a href="/setting/WithOut">本好きのあなたに送る、最高の時間を楽しもう!!</a>
               </p>
             </div>
             <Link href="/account/AccountTop">
